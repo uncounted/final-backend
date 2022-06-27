ffb43e3092456d54e8c641b8c70f97d1aac42d72
Minji Kim <qwe38280@gmail.com>
2022-06-28 00:52:03 +0900

    Revert "Revert "no message""
    This reverts commit 709193c538a53d6006cd459ec2d7f9eb0c0b05be.

--- src/main/java/com/hanghae0705/sbmoney/model/domain/SavedItem.java
+++ src/main/java/com/hanghae0705/sbmoney/model/domain/SavedItem.java
@@ -2,13 +2,17 @@ package com.hanghae0705.sbmoney.model.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.hanghae0705.sbmoney.model.domain.baseEntity.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
+@RequiredArgsConstructor
 public class SavedItem extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -19,9 +23,6 @@ public class SavedItem extends BaseEntity {
     @JoinColumn(name = "USER_ID")
     @JsonBackReference(value = "user-fk")
     User user;
-
-    @Column(nullable = false)
-    int count;
 
     @Column(nullable = false)
     int price;
@@ -35,5 +36,36 @@ public class SavedItem extends BaseEntity {
     @JoinColumn(name = "GOAL_ITEM_ID")
     @JsonBackReference(value = "goalItem-fk")
     GoalItem goalItem;
+
+    public SavedItem(Item item, int price, User user){
+        this.item = item;
+        this.price = price;
+        this.user = user;
+    }
+    
+    //User 객체를 받을 때 추후 삭제
+    public SavedItem(Item item, int price){
+        this.item = item;
+        this.price = price;
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request {
+        private Long itemId;
+        private int price;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response {
+        private Long categoryId;
+        private String categoryName;
+        private Long itemId;
+        private String itemName;
+        private int price;
+    }
 
 }
--- src/main/java/com/hanghae0705/sbmoney/repository/SavedItemRepository.java
+++ src/main/java/com/hanghae0705/sbmoney/repository/SavedItemRepository.java
@@ -1,2 +1,7 @@
-package com.hanghae0705.sbmoney.repository;public interface SavedItemRepository {
+package com.hanghae0705.sbmoney.repository;
+
+import com.hanghae0705.sbmoney.model.domain.SavedItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SavedItemRepository extends JpaRepository<SavedItem, Long> {
 }
