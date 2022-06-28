cd61a38ff07062e683f2c52d610d79893279cfc5
Minji Kim <qwe38280@gmail.com>
2022-06-28 13:31:10 +0900

    목표 항목 등록

--- src/main/java/com/hanghae0705/sbmoney/model/domain/GoalItem.java
+++ src/main/java/com/hanghae0705/sbmoney/model/domain/GoalItem.java
@@ -3,7 +3,10 @@ package com.hanghae0705.sbmoney.model.domain;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.hanghae0705.sbmoney.model.domain.baseEntity.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@RequiredArgsConstructor
 public class GoalItem extends BaseEntity {
     @Column(name = "GOAL_ITEM")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +28,9 @@ public class GoalItem extends BaseEntity {
 
     @Column
     LocalDateTime reachedAt;
+
+    @Column
+    String image;
 
     @Column(nullable = false)
     int count;
@@ -45,6 +52,32 @@ public class GoalItem extends BaseEntity {
 
     @Column
     double goalPercent;
+
+    public GoalItem(User user, int count, int total, Item item) {
+        this.user = user;
+        this.count = count;
+        this.total = total;
+        this.item = item;
+    }
+
+    public void setGoalPercent(double goalPercent){
+        this.goalPercent = goalPercent;
+    }
+
+    public void setCheckReached(boolean checkReached){
+        this.checkReached = checkReached;
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request {
+        private Long categoryId;
+        private Long itemId;
+        private int goalItemCount;
+        private int price;
+    }
 
 
 }
--- src/main/java/com/hanghae0705/sbmoney/repository/ItemRepository.java
+++ src/main/java/com/hanghae0705/sbmoney/repository/ItemRepository.java
@@ -4,4 +4,5 @@ import com.hanghae0705.sbmoney.model.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findByCategoryIdAndId(Long categoryId, Long itemId);
 }
