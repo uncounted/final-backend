73628e28dde84a6d66f66a734d78d354e48f08b3
WalGoo <hyr1228@naver.com>
2022-07-07 17:28:33 +0900

    사진 업로드 추가

--- src/main/java/com/hanghae0705/sbmoney/service/MyProfileService.java
+++ src/main/java/com/hanghae0705/sbmoney/service/MyProfileService.java
@@ -7,26 +7,36 @@ import com.hanghae0705.sbmoney.model.domain.User;
 import com.hanghae0705.sbmoney.repository.UserRepository;
 import com.hanghae0705.sbmoney.security.SecurityUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
 
 @Service
 public class MyProfileService {
 
     private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
 
-    public MyProfileService(UserRepository userRepository) {
+    public MyProfileService(UserRepository userRepository, S3Uploader s3Uploader) {
         this.userRepository = userRepository;
+        this.s3Uploader = s3Uploader;
     }
 
     @Transactional
-    public Message updateProfile(User.RequestProfile requestProfile) {
+    public Message updateProfile(User.RequestProfile requestProfile, MultipartFile profileImg) throws IOException {
         User user = userRepository.findByUsername(SecurityUtil.getCurrentUsername()).orElseThrow(
                 () -> new ApiRequestException(ApiException.NOT_EXIST_USER));
-        user.updateProfile(requestProfile);
+        String imgUrl = null;
+        if(profileImg != null){
+            imgUrl = s3Uploader.upload(profileImg, "static");
+            user.updateProfile(requestProfile, imgUrl);
+        } else {
+            user.updateProfile(requestProfile);
+        }
         User.ResponseProfile responseProfile = new User.ResponseProfile();
         responseProfile.setEmail(user.getEmail());
-        responseProfile.setProfileImg(user.getProfileImg());
+        responseProfile.setProfileImg(imgUrl);
         responseProfile.setIntroDesc(user.getIntroDesc());
         responseProfile.setNickname(user.getNickname());
         return new Message(true, "프로필이 변경되었습니다", responseProfile);
