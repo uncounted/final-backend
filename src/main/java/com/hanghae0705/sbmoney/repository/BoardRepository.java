0fd298442019102ea0a34d9b5a67813bb2d5d135
Minji Kim <qwe38280@gmail.com>
2022-07-12 23:25:38 +0900

    무한 스크롤 구현

--- src/main/java/com/hanghae0705/sbmoney/repository/BoardRepository.java
+++ src/main/java/com/hanghae0705/sbmoney/repository/BoardRepository.java
@@ -1,8 +1,11 @@
 package com.hanghae0705.sbmoney.repository;
 
 import com.hanghae0705.sbmoney.model.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Board findAllById(Long id);
+    //lastBoardId보다 작은 값의 id의 게시물을 수정일자 순으로 가져온다.
+    Page<Board> findByIdLessThanOrderByIdDesc(Long lastBoardId, Pageable pageable);
 }
