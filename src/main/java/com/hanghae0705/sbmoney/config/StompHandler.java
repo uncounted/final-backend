b27d43198cd583e2e24f54709d1b0e333922d0a2
Java-kokyu <qwe38280@gmail.com>
2022-07-26 00:32:46 +0900

    no message

--- src/main/java/com/hanghae0705/sbmoney/config/StompHandler.java
+++ src/main/java/com/hanghae0705/sbmoney/config/StompHandler.java
@@ -41,11 +41,7 @@ public class StompHandler implements ChannelInterceptor {
             redisChatRoomRepository.setUserEnterInfo(sessionId, roomId);
             redisChatRoomRepository.plusUserCount(roomId);
             // 클라이언트 입장 메시지를 채팅방에 발송한다.(redis publish)
-            String nickname = Optional.ofNullable((String) message.getHeaders().get("sender")).orElse("UnknownUser");
-            String profileImg = Optional.ofNullable((String) message.getHeaders().get("profileImg")).orElse("UnknownUser");
-            //String name = Optional.ofNullable((Principal) message.getHeaders().get("simpUser")).map(Principal::getName).orElse("UnknownUser");
-            chatService.sendChatMessage(ChatMessage.builder().type(ChatMessage.MessageType.ENTER).roomId(roomId).sender(nickname).profileImg(profileImg).build());
-            log.info("SUBSCRIBED {}, {}", nickname, roomId);
+           log.info("SUBSCRIBED, {}", roomId);
         } else if (StompCommand.DISCONNECT == accessor.getCommand()) { // Websocket 연결 종료
             // 연결이 종료된 클라이언트 sesssionId로 채팅방 id를 얻는다.
             String sessionId = (String) message.getHeaders().get("simpSessionId");
