9e79217409e242908f8e3a769cbfea0765764e6d
Java-kokyu <qwe38280@gmail.com>
2022-07-26 18:23:14 +0900

    Socket Origin https://www.tikkeeul.com 추가

--- src/main/java/com/hanghae0705/sbmoney/config/WebSockConfig.java
+++ src/main/java/com/hanghae0705/sbmoney/config/WebSockConfig.java
@@ -23,7 +23,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chatting").setAllowedOrigins("http://localhost:3000")
+        registry.addEndpoint("/chatting").setAllowedOrigins("https://www.tikkeeul.com", "http://localhost:3000")
                 .withSockJS(); // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
     }
 
