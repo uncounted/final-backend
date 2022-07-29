936e35493624044f0e9ab8226d0fb21a370ccbc4
hoholoudly <hoholoudly@gmail.com>
2022-07-29 19:29:26 +0900

    FP-150 프로필 이미지 수정

--- src/main/java/com/hanghae0705/sbmoney/security/SecurityConfig.java
+++ src/main/java/com/hanghae0705/sbmoney/security/SecurityConfig.java
@@ -92,6 +92,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //Oauth2
                 .and()
                 .oauth2Login()
+                .loginPage("/oauth")
                 //.and()
                 .userInfoEndpoint() // 로그인 성공 후 사용자 정보 가져올 때의 설정
                 .userService(oauth2UserService)
@@ -136,7 +137,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/oauth2/redirect/**",
             "/login/oauth2/**",
             "/api/statistics/**",
-            "/chatting/**"
+            "/chatting/**",
+            "/login/**"
             //"/api/chat/**"
             //"/api/goalItem"
     };
