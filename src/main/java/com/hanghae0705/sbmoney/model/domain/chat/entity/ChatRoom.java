fee8786b61cfdef4b7c00d72aa5ccf04b6ab84b4
hoholoudly <hoholoudly@gmail.com>
2022-08-01 22:26:29 +0900

    FP-150 찬반 퍼센트 int -> float 수정

--- src/main/java/com/hanghae0705/sbmoney/model/domain/chat/entity/ChatRoom.java
+++ src/main/java/com/hanghae0705/sbmoney/model/domain/chat/entity/ChatRoom.java
@@ -161,17 +161,17 @@ public class ChatRoom extends CreatedTime {
 
         @Builder
         public ClosedResponse(ChatRoom chatRoom) {
-            int totalCount = chatRoom.getVoteTrueCount() + chatRoom.getVoteFalseCount();
+            float totalCount = chatRoom.getVoteTrueCount() + chatRoom.getVoteFalseCount();
 
             this.roomId = chatRoom.getRoomId();
             this.comment = chatRoom.getComment();
             this.authorNickname = chatRoom.getUser().getNickname();
             this.authorProfileImg = chatRoom.getUser().getProfileImg();
-            this.voteTruePercent = (totalCount == 0) ? 0 : chatRoom.getVoteTrueCount() / totalCount * 100;
-            this.voteFalsePercent = (totalCount == 0) ? 0 : chatRoom.getVoteFalseCount() / totalCount * 100;
+            this.voteTruePercent = (totalCount == 0) ? 0 : Math.round(chatRoom.getVoteTrueCount() / totalCount * 100);
+            this.voteFalsePercent = (totalCount == 0) ? 0 : Math.round(chatRoom.getVoteFalseCount() / totalCount * 100);
             this.createdAt = chatRoom.getCreatedDate();
         }
-    }
+    }//Math.round(chatRoom.getVoteTrueCount() / totalCount * 100);
 
 
     @Getter
